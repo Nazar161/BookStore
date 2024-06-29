3dce13903c56d355f709dd7a53916db1bd6c950f
nazaret <nazaret155@mail.ru>
2024-06-29 16:26:57 +0300

    init

---
+++ BookStore.API/Contracts/BooksResponse.cs
@@ -0,0 +1,5 @@
+namespace BookStore.Contracts;
+
+public record BooksResponse(Guid Id, string Title, string Description, decimal Price);
+
+public record BooksRequest(string Title, string Description, decimal Price);
